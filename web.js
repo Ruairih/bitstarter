d24df29971d89e18669959472ef6291d3ce21280
J. Bond <DoubleOh7@MISix.com>
2013-07-07 18:19:58 +0000

    Adding part1

--- web.js
+++ web.js
@@ -1,13 +1,17 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var filebuf =fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(filebuf.toString('utf-8'));
 });
+
+filebuf.toString('utf-8')
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
